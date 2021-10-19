a96d255431c040395ab1adb6e28fe7f0600ca0af
dimasmeisabraditya <dimasmeisabraditya@gmail.com>
2021-10-19 16:42:38 +0700

    add authentication

--- main.py
+++ main.py
@@ -1,23 +1,149 @@
 import json
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from typing import Optional
+from datetime import datetime, timedelta
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+user_dataBase = {
+    "asdf" : {
+        "username": "asdf",
+        "full_name": "ASDF",
+        "hashed_password": "$2b$12$hFUIWu7gC7gqrr8of1XNj.P5DFE5GYvibPKNyKyvrfSTxgqAyPicC",
+        "disabled": False
+    },
+}
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class Item(BaseModel):
+    id: int
+    nama: str
+
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+class UserInDB(User):
+    hashed_password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 with open("menu.json", "r") as read_file:
     data = json.load(read_file)
 app = FastAPI()
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(user_dataBase, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(user_dataBase, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
 
 @app.get('/')
 def root():
     return{'Menu':'Item'}
 
 @app.get('/menu')
-async def read_AllMenu():
+async def read_AllMenu(current_user: User = Depends(get_current_active_user)):
     return data
 
 
 @app.get('/menu/{item_id}')
-async def read_menu(item_id: int):
+async def read_menu(item_id: int, current_user: User = Depends(get_current_active_user)):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             return menu_item
@@ -26,7 +152,7 @@ async def read_menu(item_id: int):
     )
 
 @app.post('/menu')
-async def post_menu(name:str):
+async def post_menu(name:str, current_user: User = Depends(get_current_active_user)):
     id=1
     if(len(data['menu'])>0):
         id=data['menu'][len(data['menu'])-1]['id']+1
@@ -43,7 +169,7 @@ async def post_menu(name:str):
     )
 
 @app.put('/menu/{item_id}')
-async def update_menu(item_id: int, name:str):
+async def update_menu(item_id: int, name:str, current_user: User = Depends(get_current_active_user)):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             menu_item['name']=name
@@ -59,7 +185,7 @@ async def update_menu(item_id: int, name:str):
     )
 
 @app.delete('/menu/{item_id}')
-async def delete_menu(item_id: int):
+async def delete_menu(item_id: int, current_user: User = Depends(get_current_active_user)):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             data['menu'].remove(menu_item)
